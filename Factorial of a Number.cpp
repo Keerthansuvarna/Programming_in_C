fae47e5ba03a0b041b1c43b5f8ccac286f25e1d9
KEERTHAN SUVARNA <66198801+Keerthansuvarna@users.noreply.github.com>
2022-06-28 20:40:13 +0530

    Add files via upload

---
+++ Factorial of a Number.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+/*Code to find Factorial of a number*/
+using namespace std;
+int main()
+{
+	int number = 0;
+	int i=0;
+	/*Factorial of ZERO, 0!=1, hence initialize the factorial value to 1*/
+	int factorial = 1;
+	cout << "Enter the number" << endl;
+	cin >> number;
+
+	/*Check whether the number less than ZERO*/
+
+	if (number < 0)
+	{
+		/*If the number is negative then print the error and terminate the program from further execution*/
+		cout << "Cannot find the factorial for negative numbers" << endl;
+		/*Terminate the execution*/
+		return 0;
+	}
+     i=1;
+	/*TO the factorial of any given number if it is not a negative number */
+		while (i <= number)
+		{
+			factorial *= i; //factorial = factorial * i
+			i++;
+		}
+
+		cout << "Factorial of  " << number << "  is :  " << factorial << endl;
+
+	return 0;
+}
