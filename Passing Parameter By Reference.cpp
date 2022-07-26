5978d43534609ea87a63cbf0aefcef852e18b494
KEERTHAN SUVARNA <66198801+Keerthansuvarna@users.noreply.github.com>
2022-07-26 21:31:05 +0530

    Update Passing Parameter By Reference.cpp

--- Passing Parameter By Reference.cpp
+++ Passing Parameter By Reference.cpp
@@ -3,8 +3,8 @@ using namespace std;
                                                          //Passing parameter by reference  - Swapping 2 Nos. using function
 /*In function we can return or get only one value. The function will not update the actual values if we give the same name also.*/
 /*This tutorial will explain how to update the actual values using passing parameters by reference i.e manipulate the values in its memory location itself*/
-// no need to provide same name as passing parameter in the main function. (i.e, in this a & b)
 
+// no need to provide same name as passing parameter in the main function. (i.e, in this a & b)
 void swap(int &c, int &d)
 {
     int temp = c;
